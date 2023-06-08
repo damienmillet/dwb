7fbd60626f870e66884d0ec7d7e255b96abadab4
damienmillet <contact@damien-millet.dev>
2023-06-08 12:43:00 +0000

    ➕ add ssh

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,25 @@ WORKDIR /app
 
 VOLUME /app
 
+RUN adduser -D dwb
+
+VOLUME /home/dwb/.ssh
+
 EXPOSE 80 8000 3000 8080 443 3306
 
-RUN apk add --no-cache git npm composer nano
+RUN apk add --no-cache git npm composer nano openssh
+RUN ssh-keygen -A
 
-# RUN ln -s $(/usr/bin/${php_version}) /usr/bin/php
-
-# COPY entrypoint.sh /
 COPY /scripts/* /dwb/scripts/
 COPY /config/* /dwb/config/
 
+# RUN sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+# RUN sed -i "s/#PermitEmptyPasswords no/PermitEmptyPasswords yes/g" /etc/ssh/sshd_config
+
+RUN echo "ssh is open at :" $(hostname -i)
+RUN echo "port :" $(cat /etc/ssh/sshd_config | grep -E '#Port\s(\d.*)' | tail -n 1 | awk '{print $2}')
+
 RUN chmod +x /dwb/scripts/*
 # ENTRYPOINT ["/dwb/entrypoint.sh"]
+CMD ["/usr/sbin/sshd", "< /dev/null &"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # This script checks if the container is started for the first time.
 
-CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
-if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
-    touch /$CONTAINER_FIRST_STARTUP
-    # place your script that you only want to run on first startup.
-    npm run initdb && npm run dev
-else
-    # script that should run the rest of the times (instances where you 
-    # stop/restart containers).
-    npm run dev
-fi
+# CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+# if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+#     touch /$CONTAINER_FIRST_STARTUP
+#     # place your script that you only want to run on first startup.
+#     npm run initdb && npm run dev
+# else
+#     # script that should run the rest of the times (instances where you 
+#     # stop/restart containers).
+#     npm run dev
+# fi
 
 # github cli # needed for private repos
 
